992297e3914c7e0c5de37550886ce5774ea28f9e
DannoPeters <Danno.Peters@usask.ca>
2019-05-06 11:52:35 -0600

    Created Python Script to Parse JSON Files to SQLite

---
+++ RTI/realtime-chart-JSON_2_SQLite.py
@@ -0,0 +1,8 @@
+# Author: Danno Peters, Danno@dannopeters.ca
+# Language Python 2.7
+# Purpose: Upkeeps an SQLite database of SuperDARN Chart Data
+# Input: JSON files from RADARs
+# Output: Populated SQLite Database
+
+# Variables
+	# 
