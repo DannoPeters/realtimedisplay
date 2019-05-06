7dd545b4159fdb5039b28d70594fd7105aea3c92
DannoPeters <Danno.Peters@usask.ca>
2019-05-06 13:51:03 -0600

    Added some libraries and basic program structure

--- RTI/realtime-chart-JSON_2_SQLite.py
+++ RTI/realtime-chart-JSON_2_SQLite.py
@@ -4,5 +4,28 @@
 # Input: JSON files from RADARs
 # Output: Populated SQLite Database
 
+#import required libaraies
+import sqlite #allows for interation with SQLite databases
+import sys
+import pathlib
+import json #allows python to parse JSON files
+import asyncio #used for asynchronous communication (WSS)
+import websockets #used tyo connect to chapman server websockets
+
 # Variables
-	# 
+	database = 'realtime-charts.sqlite' #set name of SQLite database to be found or created
+	sqlPath = PurePath('') #set path to SQLite Database
+	dataSpan = 1 #length of time to store data (Hours)
+
+#Check if database exists, if not, create it
+
+#Connect to Web sockets
+
+while 1 {
+
+#Parse JSON File
+
+#Add new Data to SQLite Database
+
+#Remove old Data from SQLite Database
+}
