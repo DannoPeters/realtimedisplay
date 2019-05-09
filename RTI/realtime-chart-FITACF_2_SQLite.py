4e37945a5389014941defcab871b712c315f5134
DannoPeters <Danno.Peters@usask.ca>
2019-05-09 10:19:01 -0600

    Able to create and open databases.
    ToFix: Read database structure in Python, and Create database structure

--- RTI/realtime-chart-FITACF_2_SQLite.py
+++ RTI/realtime-chart-FITACF_2_SQLite.py
@@ -1,29 +1,59 @@
 # Author: Danno Peters, Danno@dannopeters.ca
 # Language Python 2.7
 # Purpose: Upkeeps an SQLite database of SuperDARN Chart Data
-# Input: JSON files from RADARs
+# Input: JFITACF files from RADARs
 # Output: Populated SQLite Database
+
+
+#Functions
+
 
 #import required libaraies
 import sqlite3 #allows for interation with SQLite databases
 import sys
 import os
-import pathlib
-import asyncio #used for asynchronous communication (WSS)
-import socket #used to connect to chapman server websockets
+
 
 # Variables
-	db_name = 'realtime-charts.sqlite' #set name of SQLite database to be found or created
-	sqlPath = PurePath('') #set path to SQLite Database
-	dataSpan = 1 #length of time to store data (Hours)
-
-#Check if database exists, if not, create it (easy in python 3.4+)
-db_exists = not os.path.exists(db_name)
+db_name = 'SuperDARN RealTimeChart.sql' #set name of SQLite database to be found or created
+#db_schema = 
+dataSpan = 1 #length of time to store data (Hours)
 connection = sqlite3.connect(db_name)
 
-if db_exists:
-    print('No schema exists.')
+def check_db(db_name):
+    if exists_db(db_name):
+        cursor = connection.cursor()
+        db_tables = cursor.execute("SELECT name FROM sqlite_master WHERE type='table';")
+        for table in db_tables:
+            print(table)
+    else:
+        print('Message: ', db_name, ' Does Not Exist')
+
+
+#exists_db: checks if a database currently exists
+    #Input: db_name - name of the database ie 'realtime-charts.sqlite'
+    #Output: exists - bool, True if database exists, False if it does not
+def exists_db(db_name):
+    exists = os.path.exists(db_name)
+    return exists
+
+
+if exists_db(db_name):
+    print('Message: ', db_name, ' Exists')
+    check_db(db_name)
 else:
-    print('DB exists.')
+    print('Message: ', db_name, ' Does Not Exist')
 
 connection.close()
+
+#create_db: creates a SQLite database with user defined name and schema
+#   Input:  db_name - name of the database ie 'realtime-charts.sqlite'
+#           db_schema - expected databse schema ***need to decide on format***
+#   Output: 
+#def create_db(db_name, db_schema):
+    
+
+#check_db: compares current database structure to expected schema
+#   Input:  db_name - name of the database ie 'realtime-charts.sqlite'
+#           db_schema - expected databse schema ***need to decide on format***
+#   Output:
